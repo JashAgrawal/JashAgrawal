3b9350b2430bfcf75666491817c3547c05f41360
Chirayu Agrawal <Chirayu.agrawal4000@gmail.com>
2024-06-10 03:39:39 +0530

    add project links and new resume

--- next.config.js
+++ next.config.js
@@ -14,9 +14,35 @@ const nextConfig = {
         },
         {
           source:"/resume",
-          destination:'https://drive.google.com/file/d/1Vk3leP9pnI4X5ERl9XOym4garRRbstEY/view?usp=sharing',
+          destination:'https://drive.google.com/file/d/1NLJ-RXEM3u-oVF-o4wlwehA8pBs6tb9K/view?usp=sharing',
           permanent: true
-        }
+        },
+        {
+          source:"/autobackend",
+          destination:'https://autobackend.vercel.app/',
+          permanent: true
+        },
+        {
+          source:"/cjflixx",
+          destination:'https://cjflixx.vercel.app/',
+          permanent: true
+        },
+        {
+          source:"/vangaurd-vogue",
+          destination:'https://vangaurd-vogue.vercel.app/',
+          permanent: true
+        },
+        {
+          source:"/ielts-wizardz",
+          destination:'https://www.ieltswizardz.com/',
+          permanent: true
+        },
+        {
+          source:"/sp-mills",
+          destination:'https://suryaprakashsilkmills.com/',
+          permanent: true
+        },
+
       ]
   },
   images: {
